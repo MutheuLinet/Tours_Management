43a2807d1d0329ae9dbb25cd479c3276bb273269
MutheuLinet <muneenilinet@gmail.com>
2022-08-14 10:25:12 +0300

    Inject TourPackageRepository into TourPackageService, autowire to the constructor, add 3 methods to createTourPackage, lookup all tour packages and return total number of tour packages

---
+++ src/main/java/com/lyn/ec/service/TourPackageService.java
@@ -0,0 +1,45 @@
+package com.lyn.ec.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lyn.ec.domain.TourPackage;
+import com.lyn.ec.repo.TourPackageRepository;
+
+
+@Service
+public class TourPackageService {
+	
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a Tour Package
+     *
+     * @param code code of the package
+     * @param name name of the package
+     *
+     * @return new or existing tour package
+     */
+    
+    public TourPackage createTourPackage(String code, String name) {
+        return tourPackageRepository.findById(code).orElse(tourPackageRepository.save(new TourPackage(code, name)));
+    }
+
+    /**
+     * Lookup All Tour packages
+     *
+     * @return
+     */
+    public Iterable<TourPackage> lookup(){
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+}
