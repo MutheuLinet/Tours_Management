ad1882d368257750c04bca8df22e2b93997c03e7
MutheuLinet <muneenilinet@gmail.com>
2022-08-15 19:26:48 +0300

    Runtime db initialisation

--- src/main/java/com/lyn/ec/ExploreCaliApplication.java
+++ src/main/java/com/lyn/ec/ExploreCaliApplication.java
@@ -42,97 +42,97 @@ public class ExploreCaliApplication implements CommandLineRunner {
         // set up database before permitting web requests
         // run method at object scoped therefore we can access the injected services
         // autowired above
-//        createTourAllPackages();
-//        createTours(importFile);
+        createTourAllPackages();
+        createTours(importFile);
     }
 
     /**
      * Initialize all the known tour packages
      */
-//    private void createTourAllPackages() {
-//        tourPackageService.createTourPackage("BC", "Backpack Cal");
-//        tourPackageService.createTourPackage("CC", "California Calm");
-//        tourPackageService.createTourPackage("CH", "California Hot springs");
-//        tourPackageService.createTourPackage("CY", "Cycle California");
-//        tourPackageService.createTourPackage("DS", "From Desert to Sea");
-//        tourPackageService.createTourPackage("KC", "Kids California");
-//        tourPackageService.createTourPackage("NW", "Nature Watch");
-//        tourPackageService.createTourPackage("SC", "Snowboard Cali");
-//        tourPackageService.createTourPackage("TC", "Taste of California");
-//    }
-//
-//    /**
-//     * Create tour entities from an external file
-//     */
-//    private void createTours(String fileToImport) throws IOException {
-//        TourFromFile.read(fileToImport).forEach(importedTour -> tourService.createTour(importedTour.getTitle(),
-//                importedTour.getDescription(),
-//                importedTour.getBlurb(),
-//                importedTour.getPrice(),
-//                importedTour.getLength(),
-//                importedTour.getBullets(),
-//                importedTour.getKeywords(),
-//                importedTour.getPackageType(),
-//                importedTour.getDifficulty(),
-//                importedTour.getRegion()));
-//    }
-//
-//    /**
-//     * Helper class to import ExploreCalifornia.json
-//     */
-//    private static class TourFromFile {
-//        // fields
-//        private String packageType, title, description, blurb, price, length,
-//                bullets, keywords, difficulty, region;
-//
-//        // reader
-//        static List<TourFromFile> read(String fileToImport) throws IOException {
-//            return new ObjectMapper().setVisibility(FIELD, ANY).readValue(new FileInputStream(fileToImport),
-//                    new TypeReference<List<TourFromFile>>() {
-//                    });
-//        }
-//
-//        protected TourFromFile() {
-//        }
-//
-//        String getPackageType() {
-//            return packageType;
-//        }
-//
-//        String getTitle() {
-//            return title;
-//        }
-//
-//        String getDescription() {
-//            return description;
-//        }
-//
-//        String getBlurb() {
-//            return blurb;
-//        }
-//
-//        Integer getPrice() {
-//            return Integer.parseInt(price);
-//        }
-//
-//        String getLength() {
-//            return length;
-//        }
-//
-//        String getBullets() {
-//            return bullets;
-//        }
-//
-//        String getKeywords() {
-//            return keywords;
-//        }
-//
-//        Difficulty getDifficulty() {
-//            return Difficulty.valueOf(difficulty);
-//        }
-//
-//        Region getRegion() {
-//            return Region.findByLabel(region);
-//        }
-//    }
+    private void createTourAllPackages() {
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("CH", "California Hot springs");
+        tourPackageService.createTourPackage("CY", "Cycle California");
+        tourPackageService.createTourPackage("DS", "From Desert to Sea");
+        tourPackageService.createTourPackage("KC", "Kids California");
+        tourPackageService.createTourPackage("NW", "Nature Watch");
+        tourPackageService.createTourPackage("SC", "Snowboard Cali");
+        tourPackageService.createTourPackage("TC", "Taste of California");
+    }
+
+    /**
+     * Create tour entities from an external file
+     */
+    private void createTours(String fileToImport) throws IOException {
+        TourFromFile.read(fileToImport).forEach(importedTour -> tourService.createTour(importedTour.getTitle(),
+                importedTour.getDescription(),
+                importedTour.getBlurb(),
+                importedTour.getPrice(),
+                importedTour.getLength(),
+                importedTour.getBullets(),
+                importedTour.getKeywords(),
+                importedTour.getPackageType(),
+                importedTour.getDifficulty(),
+                importedTour.getRegion()));
+    }
+
+    /**
+     * Helper class to import ExploreCalifornia.json
+     */
+    private static class TourFromFile {
+        // fields
+        private String packageType, title, description, blurb, price, length,
+                bullets, keywords, difficulty, region;
+
+        // reader
+        static List<TourFromFile> read(String fileToImport) throws IOException {
+            return new ObjectMapper().setVisibility(FIELD, ANY).readValue(new FileInputStream(fileToImport),
+                    new TypeReference<List<TourFromFile>>() {
+                    });
+        }
+
+        protected TourFromFile() {
+        }
+
+        String getPackageType() {
+            return packageType;
+        }
+
+        String getTitle() {
+            return title;
+        }
+
+        String getDescription() {
+            return description;
+        }
+
+        String getBlurb() {
+            return blurb;
+        }
+
+        Integer getPrice() {
+            return Integer.parseInt(price);
+        }
+
+        String getLength() {
+            return length;
+        }
+
+        String getBullets() {
+            return bullets;
+        }
+
+        String getKeywords() {
+            return keywords;
+        }
+
+        Difficulty getDifficulty() {
+            return Difficulty.valueOf(difficulty);
+        }
+
+        Region getRegion() {
+            return Region.findByLabel(region);
+        }
+    }
 }
