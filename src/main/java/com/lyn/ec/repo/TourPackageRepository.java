1ce7a00d4f12376e931da1e47cbdcce3c85610e4
MutheuLinet <muneenilinet@gmail.com>
2022-08-15 09:34:22 +0300

    @Override & @RestResource features to stop public from modifying your database

--- src/main/java/com/lyn/ec/repo/TourPackageRepository.java
+++ src/main/java/com/lyn/ec/repo/TourPackageRepository.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.lyn.ec.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     /**
      * Find Tour Package by name.
@@ -16,4 +19,56 @@ public interface TourPackageRepository extends CrudRepository<TourPackage, Strin
      */
     
     Optional<TourPackage> findByName(@Param("name")String name);
+
+    //@Override & @RestResource features to stop public from modifying your database
+    
+//	@Override
+//	@RestResource(exported=false) //method level annotation
+//	default <S extends TourPackage> S save(S entity) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	@RestResource(exported=false)
+//	default <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteById(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void delete(TourPackage entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAllById(Iterable<? extends String> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll(Iterable<? extends TourPackage> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+    
 }
